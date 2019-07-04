4bd9574f118109cc54c873b113886405c231b7b8
Luiz Batanero <luizbatanero@gmail.com>
2019-07-04 00:28:05 -0300

    Fetch starred repos from User

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User() {
-  return <View />;
+export default class User extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    return <View />;
+  }
 }
