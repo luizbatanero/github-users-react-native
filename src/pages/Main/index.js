f3c61a70aa93b4ef436fa49c0842f089e65f9ecd
Luiz Batanero <luizbatanero@gmail.com>
2019-07-03 23:30:35 -0300

    Style Main form with styled components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = {
